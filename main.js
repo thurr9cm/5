d394d1c64747e855b249b2c360ee6cfddb2ffb23
thurr9cm <143123282+thurr9cm@users.noreply.github.com>
2024-11-04 08:16:21 -0300

    fgsb\dsvdbzd

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-<div class="conteudo-cartao">
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+        <div class="conteudo-cartao">
             <h3>${categoria}</h3>
             <div class="pergunta-cartao">
                 <p>${pergunta}</p>
