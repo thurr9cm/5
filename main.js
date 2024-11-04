5ec4efd6ae2996b1699812d7f370913a8a5f1736
thurr9cm <143123282+thurr9cm@users.noreply.github.com>
2024-11-04 08:09:49 -0300

    gfhhfd

---
+++ main.js
@@ -0,0 +1,20 @@
+<div class="conteudo-cartao">
+            <h3>${categoria}</h3>
+            <div class="pergunta-cartao">
+                <p>${pergunta}</p>
+            </div>
+            <div class="resposta-cartao">
+                <p>${resposta}</p>
+            </div>
+        </div>
+    `;
+
+    let respostaEstaVisivel = false;
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener('click', viraCartao);
+    container.appendChild(cartao);
+}
